ab56a5c2491f46199cc642bd2e6ce99f601f44c7
guilherme.leles <guilherme.leles@concert.com.br>
2022-03-30 10:13:38 -0300

    bluetooth interrupt working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -31,6 +31,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define RXBUFFERSIZE                     6
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -39,23 +40,32 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+uint8_t UART1_rxBuffer[RXBUFFERSIZE] = {0};
+uint8_t bluetooth_rxBuffer[RXBUFFERSIZE] = {0};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  if(huart->Instance==USART1){
+    HAL_UART_Receive_IT(&huart1,UART1_rxBuffer,RXBUFFERSIZE);
+    memcpy(bluetooth_rxBuffer, UART1_rxBuffer, RXBUFFERSIZE * sizeof(uint8_t));
+  }
+}
 /* USER CODE END 0 */
 
 /**
@@ -87,8 +97,9 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_UART_Receive_IT(&huart1,UART1_rxBuffer,RXBUFFERSIZE);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -144,6 +155,39 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
 }
 
 /**
@@ -213,7 +257,14 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+void Debug(uint8_t *ch, size_t numElements)
+{
+	HAL_UART_Transmit(&huart2,ch,numElements,10);
+}
+void Send_Bluettoh_Data(uint8_t *ch, size_t numElements)
+{
+	HAL_UART_Transmit(&huart1,ch,numElements,10);
+}
 /* USER CODE END 4 */
 
 /**
